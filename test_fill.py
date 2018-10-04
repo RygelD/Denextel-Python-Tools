163523d1dbc95062b5f237475423ce29dd0c7082
RygelD <43830270+RygelD@users.noreply.github.com>
2018-10-04 09:27:29 -0400

    Indev Layer
    Layer class (to debug), ptop in dev

--- test_fill.py
+++ test_fill.py
@@ -1,7 +1,31 @@
 from dpged import *
+__version__ = '0.1.0-alpha'
 def fill(item,xytocolour):
-  #Fill: Item = canvas name, xycolour is {(x,y):(r,g,b)}
+  #Fill: Item = canvas name, xycolour is [ ((x,y),(r,g,b)) , ((),())]
   fn = xytocolour
   for i in fn:
-    j = fn[i]
-    item.drawpx(i,j)
+    j = i[1]
+    item.drawpx(i[0],j)
+sets = []
+class Layer:
+    def __init__(self,priority,xytocolour):
+      self.p = priority
+      self.i = item
+      self.s = xytocolour
+      sets.append(self)
+    def setpx(self,xy,rgb):
+      comp = []
+      for i in self.s:
+        comp.append([self.s[0],i])
+      for j in list(reversed(comp)):
+        if j[0] == xy:
+          self.s.remove(j[1])
+      self.s.append([xy,rgb])
+      
+def ptop(item):
+  fights = []
+  for i in sets:
+    pixels = i.s
+    
+    comp = ()
+    
