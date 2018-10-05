5730dd8ca3ec1876ebc01f0f4ddf723db2aac0cc
RygelD <43830270+RygelD@users.noreply.github.com>
2018-10-04 21:55:22 -0400

    Create test2.py

---
+++ debug/test2.py
@@ -0,0 +1,13 @@
+# Test picture, for v0.2.4.2 of dpged
+# 2018 Denextel, 2018 Rygel
+def test():
+    cat = mkcanvas(300,300,(200,200),screenname='FXL')
+    for i in range(0,300):
+        cat.drawpx((i,i),(255,0,255))
+        cat.drawpx((i,300-i),(255,255,0))
+        cat.drawpx((300-i,i),(0,255,255))
+        cat.drawpx((300-i,300-i),(255,0,0))
+        cat.drawpx((150,i),(0,255,0))
+        cat.drawpx((i,150),(0,0,255))
+        cat.drawpx((150,300-i),(255,255,255))
+        cat.drawpx((300-i,150),(0,0,0))
