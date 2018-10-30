5d6e9622426691d93e23a164de0f6deadcd828b7
RygelD <43830270+RygelD@users.noreply.github.com>
2018-10-30 10:03:51 -0400

    v 0.1

--- ScreenKit/import.py
+++ ScreenKit/import.py
@@ -1,4 +1,6 @@
 # Run to find code for importing ScreenKit
+__version__ = 0.1
+RELEASE = 0.1
 import sys
 import os
 print('import sys\nsys.path.insert(0,' + str(os.dirname(os.abspath(__file__))) + ')')
