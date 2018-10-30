ac86e910da376712e94640d7f99529b12533f391
RygelD <43830270+RygelD@users.noreply.github.com>
2018-10-30 09:54:27 -0400

    Create burn_Getting_Started.py

---
+++ ScreenKit/burn_Getting_Started.py
@@ -0,0 +1,10 @@
+import deletion
+import sys
+import os
+if active:
+  deletion.fdd(os.path.dirname(os.getcwd())+'GETTING_STARTED.txt','dxl')
+  deletion.fdd(sys.path,'dxl')
+else:
+  print('Not set up yet.')
+  sys.exit()
+  
